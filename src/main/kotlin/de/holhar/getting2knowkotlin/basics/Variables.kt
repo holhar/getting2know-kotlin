8f13229419cf7925e8507e7132b2a02c44158bdc
Holger Harms <holhar@posteo.de>
2022-05-26 15:53:37 +0200

    Basic Types - Int, Long, Double, String; type inference, String interpolation

--- src/main/kotlin/de/holhar/getting2knowkotlin/basics/Variables.kt
+++ src/main/kotlin/de/holhar/getting2knowkotlin/basics/Variables.kt
@@ -3,15 +3,29 @@ package de.holhar.getting2knowkotlin.basics
 fun main() {
 
     // val signifies immutables
-    val name : String = "Holger"
+    val name = "Holger"
     println(name)
 
     // Doesn't compile - not allowed for immutables
     //name = "Holger2"
 
     // var signifies mutables
-    var age : Int = 35
+    var age = 35
     println(age)
     age = 36
     println(age)
+
+    // Long value initialized by type inference
+    val salary = 30000L
+    println(salary)
+
+    // Apply String interpolation
+    val course = "Kotlin Spring"
+    println("course: $course and the course length is ${course.length}")
+
+    val multiLine = """
+        ABC
+        DEF
+    """.trimIndent() // trimIndent() removes the prefixed spaces
+    println(multiLine)
 }
