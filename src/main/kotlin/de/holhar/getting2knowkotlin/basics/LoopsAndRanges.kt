43c3f3afff90ec6cc7721ca7c8477c251b420ed9
Holger Harms <holhar@posteo.de>
2022-05-26 16:08:14 +0200

    While and do-while

--- src/main/kotlin/de/holhar/getting2knowkotlin/basics/LoopsAndRanges.kt
+++ src/main/kotlin/de/holhar/getting2knowkotlin/basics/LoopsAndRanges.kt
@@ -18,4 +18,24 @@ fun main() {
     for (i in reverseRange step 2) {
         println("reverseRange with step: $i")
     }
+
+    // WHILE
+    exploreWhile()
+    exploreDoWhile()
+}
+
+fun exploreWhile() {
+    var x = 1
+    while (x < 5) {
+        println("Value of x is: $x")
+        x++
+    }
+}
+
+fun exploreDoWhile() {
+    var i = 0
+    do {
+        println("Value of i is: $i")
+        i++
+    } while (i < 5)
 }
