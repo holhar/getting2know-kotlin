826db2a6423d95960f76569a1bbf7b14d23c2b91
Holger Harms <holhar@posteo.de>
2022-05-27 17:49:52 +0200

    Inheritance - override functions and variables

--- src/main/kotlin/de/holhar/getting2knowkotlin/classes/Inheritance.kt
+++ src/main/kotlin/de/holhar/getting2knowkotlin/classes/Inheritance.kt
@@ -3,13 +3,28 @@ package de.holhar.getting2knowkotlin.classes
 // Allow this class to be extendable by adding the 'open' keyword
 open class User(val name : String) {
 
-    fun login() {
+    // Make variable overridable by adding the 'open' keyword
+    open var isLoggedIn : Boolean = false
+
+    // Make function overridable by adding the 'open' keyword
+    open fun login() {
         println("Inside user login")
     }
 }
 
 // Student class is extending User class
-class Student(name: String) : User(name)
+class Student(name: String) : User(name) {
+
+    // Override isLoggedIn variable by adding 'override' keyword
+    override var isLoggedIn : Boolean = false
+
+    // Override login function by adding 'override' keyword
+    override fun login() {
+        println("Inside student login")
+        // Call login() function from super class
+        super.login()
+    }
+}
 
 class Instructor(name: String) : User(name)
 
@@ -17,6 +32,8 @@ fun main() {
     val student = Student("Alex")
     println("Name is: ${student.name}")
     student.login()
+    student.isLoggedIn = true
+    println("Student isLoggedIn: ${student.isLoggedIn}")
 
     val instructor = Instructor("Dilip")
     println("Name is: ${instructor.name}")
