21def8e6249146e0debdfc67f2a2f3989c81710e
Holger Harms <holhar@posteo.de>
2022-05-27 17:54:47 +0200

    Object keyword for creating singletons

---
+++ src/main/kotlin/de/holhar/getting2knowkotlin/classes/Authenticate.kt
@@ -0,0 +1,14 @@
+package de.holhar.getting2knowkotlin.classes
+
+// 'object' keyword allows us to create a class and an instance of the class at the same time
+// Equivalent to a singleton pattern in Java
+object Authenticate {
+
+    fun authenticate(userName: String, password: String) {
+        println("User ${userName} authenticated")
+    }
+}
+
+fun main() {
+    Authenticate.authenticate("Holger", "123456")
+}
