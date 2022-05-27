41d86edb7ca12ecbb72857cd218b520302d566ed
Holger Harms <holhar@posteo.de>
2022-05-27 16:56:40 +0200

    Primary constructors

--- src/main/kotlin/de/holhar/getting2knowkotlin/classes/Person.kt
+++ src/main/kotlin/de/holhar/getting2knowkotlin/classes/Person.kt
@@ -1,6 +1,10 @@
 package de.holhar.getting2knowkotlin.classes
 
-class Person {
+// Primary constructor
+class Person(
+            val name : String = "",
+            val age: Int = 0
+        ) {
 
     fun action() {
         println("Person walks")
@@ -9,6 +13,11 @@ class Person {
 
 fun main() {
     // No need to use the 'new' keyword for class instantiation
-    val person = Person()
+    val person = Person("Alex", 25)
     person.action()
+    println("Name: ${person.name}; age: ${person.age}")
+
+    // Params not needed because of defined default values in primary constructor
+    val person2 = Person()
+    println("Name: ${person2.name}; age: ${person2.age}")
 }
