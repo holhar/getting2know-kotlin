bd98284c804ef842d70f195733964e134102b648
Holger Harms <holhar@posteo.de>
2022-05-27 17:38:54 +0200

    Custom getters and setters

--- src/main/kotlin/de/holhar/getting2knowkotlin/classes/Item.kt
+++ src/main/kotlin/de/holhar/getting2knowkotlin/classes/Item.kt
@@ -1,8 +1,24 @@
 package de.holhar.getting2knowkotlin.classes
 
+import java.lang.IllegalArgumentException
+
 class Item() {
 
     var name : String = ""
+    var price : Double = 0.0
+
+    get() {
+        println("Inside getter")
+        return field
+    }
+    set(value) {
+        println("Inside setter")
+        if (value >= 0.0) {
+            field = value
+        } else {
+            throw IllegalArgumentException("Negative price is not allowed!")
+        }
+    }
 
     // Secondary constructor (this() invokes primary constructor that does not take any params here)
     constructor(_name: String): this() {
@@ -15,4 +31,8 @@ fun main() {
     println("Item name: ${item.name}")
     item.name = "Samsung Galaxy S22"
     println("Item name: ${item.name}")
+
+    item.price = 10.0
+    println(item.price)
+    item.price = -10.0
 }
