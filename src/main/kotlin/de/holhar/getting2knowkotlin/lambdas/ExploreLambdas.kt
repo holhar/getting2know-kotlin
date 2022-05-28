e4b17c1979933b930a8ff7fb68fd6a54fc8ca9f3
Holger Harms <holhar@posteo.de>
2022-05-28 11:03:44 +0200

    Lambdas and higher order functions

--- src/main/kotlin/de/holhar/getting2knowkotlin/lambdas/ExploreLambdas.kt
+++ src/main/kotlin/de/holhar/getting2knowkotlin/lambdas/ExploreLambdas.kt
@@ -1,5 +1,11 @@
 package de.holhar.getting2knowkotlin.lambdas
 
+// HIGHER ORDER FUNCTION (a function that accepts another function)
+// Third argument is a function that matches the specified signature
+fun calculate(x: Int, y: Int, op: (x: Int, y: Int) -> Int) : Int {
+    return op(x,y)
+}
+
 fun main() {
 
     // The type is necessary; type inference is not available here
@@ -14,4 +20,9 @@ fun main() {
     }
     val multiplyResult = multiplyLambda(4, 5)
     println("multiplyResult: $multiplyResult")
+
+    // HIGHER ORDER FUNCTION application - the function is passed as third argument
+    //val result = calculate(2, 3, { a, b -> a * b })
+    val result = calculate(2, 3) { a, b -> a * b }
+    println("Result: $result")
 }
