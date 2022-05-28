ca5422bd93e9f18d2f519afd06e714bfcdc78e78
Holger Harms <holhar@posteo.de>
2022-05-28 11:27:14 +0200

    Map operations on collections

--- src/main/kotlin/de/holhar/getting2knowkotlin/collections/CollectionOperations.kt
+++ src/main/kotlin/de/holhar/getting2knowkotlin/collections/CollectionOperations.kt
@@ -6,9 +6,13 @@ import de.holhar.getting2knowkotlin.dataset.courseList
 
 fun main() {
     val courseList = courseList()
+
     val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPMENT }
     val designPredicate = { c: Course -> c.category == CourseCategory.DESIGN }
+
     exploreFilter(courseList, devPredicate)
+
+    exploreMap(courseList, designPredicate)
 }
 
 fun exploreFilter(
@@ -19,4 +23,14 @@ fun exploreFilter(
     courseList
         .filter { predicate.invoke(it) }
         .forEach { println("course: $it") }
+}
+
+fun exploreMap(
+    courseList: MutableList<Course>,
+    predicate: (Course) -> Boolean
+) {
+    courseList
+        .filter { predicate.invoke(it) }
+        .map { "${it.name} - ${it.category}" }
+        .forEach { println(it) }
 }
